8e65f54fc42a075b968875336d4c4225efa467e1
Anna An <arzumanyan.an@gmail.com>
2023-02-18 11:45:50 +0400

    adding styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,18 +4,32 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header>
-      <nav>
-        <Image alt="logo" />
-        <Link href="/" passHref>
-          Home
-        </Link>
-        <Link href="/events" passHref>
-          Events
-        </Link>
-        <Link href="/about-us" passHref>
-          About Us
-        </Link>
-      </nav>
+      <div className="topNav">
+        <Image src={"/images/logo.png"} width={50} height={50} alt="logo" />
+        <nav>
+          <ul>
+            <li>
+              <Link href="/" passHref>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/events" passHref>
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link href="/about-us" passHref>
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum,
+        autem.
+      </h1>
     </header>
   );
 };
